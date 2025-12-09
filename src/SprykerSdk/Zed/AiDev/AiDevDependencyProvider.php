1547e28fe15b63ea5dcde3b7f7e13b669983fec3
vgolska <veronica.golska@spryker.com>
2025-12-09 11:30:35 +0100

    removed redundant service

--- src/SprykerSdk/Zed/AiDev/AiDevDependencyProvider.php
+++ src/SprykerSdk/Zed/AiDev/AiDevDependencyProvider.php
@@ -22,24 +22,12 @@ class AiDevDependencyProvider extends AbstractBundleDependencyProvider
 
     public const string PLUGINS_MCP_TOOL = 'PLUGINS_MCP_TOOL';
 
-    public const string SERVICE_AI_DEV = 'SERVICE_AI_DEV';
-
     public const string FACADE_OMS = 'FACADE_OMS';
 
     public function provideBusinessLayerDependencies(Container $container): Container
     {
         $container = parent::provideBusinessLayerDependencies($container);
-        $container = $this->addAiDevService($container);
         $container = $this->addOmsFacade($container);
-
-        return $container;
-    }
-
-    protected function addAiDevService(Container $container): Container
-    {
-        $container->set(static::SERVICE_AI_DEV, function (Container $container) {
-            return $container->getLocator()->aiDev()->service();
-        });
 
         return $container;
     }
